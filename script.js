7db364da8eb0086b1fe43b5b0b9daf8ed8514453
sakthi362001 <sakthi362001@gmail.com>
2023-01-27 01:56:13 -1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const userScoreEl = document.
+getElementById('user-score');
+const compScoreEl = document.
+getElementById('comp-score');
+
+const userChoiceEl = document.
+getElementById('user-choice');
+const compChoiceEl = document.
+getElementById('comp-choice');
+
+const resultContainer = document.
+querySelector('.results-container');
+const resultEl = document.getElementById('result');
+
+
+
+
+
